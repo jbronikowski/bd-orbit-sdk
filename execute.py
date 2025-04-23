66cdb93ff8d92818f68f85ee9fba3eeb444ca01d
jbronikowski <josh.bronikowski@gmail.com>
2025-04-23 13:54:38 -0400

    Added requirment and send to sample

--- execute.py
+++ execute.py
@@ -1,3 +1,15 @@
 from orbitsdk import OrbitAPI
 client = OrbitAPI()
 barq = client.robots.setRobot('BarQ')
+
+
+maps = barq.missions.getSiteMaps("Map 1")
+
+waypoint1 = maps.waypointIds[0]
+
+barq.missions.sendRobot(waypoint1)
+barq.missions.dispatchRobot()
+
+
+barq.missions.sendRobotToDock()
+barq.missions.dispatchRobot()
